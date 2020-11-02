6efa52690b5cb925411a8785003c58d3d8c73666
Lois Simonca <loisgabor@gmail.com>
2020-11-02 11:09:28 -0500

    added a line of code

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
 // array of questions for user
 const inquirer = require("inquirer");
 const fs = require("fs");
-i = 0;
 
 const questions = [
   {
     type: "input",
     message: "What is your project title?",
-    name: "title",
+    answer: "title",
   },
   {
     type: "input",
     message: "Give a description of your project.",
-    name: "description",
+    answer: "description",
   },
   {
     type: "input",
     message: "Insert your table of contents",
-    name: "contents",
+    answer: "contents",
   },
   {
     type: "input",
     message: "Input installation guidelines",
-    name: "installation",
+    answer: "installation",
   },
   {
     type: "input",
     message: "Insert instructions for use",
-    name: "usage",
+    answer: "usage",
   },
   {
     type: "input",
     message: "Insert collaborator with links to their GitHub profile",
-    name: "credits",
+    answer: "credits",
   },
   {
     type: "input",
     message: "Insert your license",
-    name: "license",
+    answer: "license",
   },
 ];
 
 // function to write README file
-fs.writeFile("README2.md", questions[0].message, (err) =>
+fs.writeFile("README2.md", questions.answer, (err) =>
   err ? console.log(err) : console.log("Success")
 );
 
 // function to initialize program
 function init() {}
 
-inquirer.prompt(questions);
+inquirer.prompt(questions).then(function (answer) {});
+
+console.log(answer);
 
 // function call to initialize program
 init();
