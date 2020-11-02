39af6b9e06547ee0051bad19efc491ced023ea94
Lois Simonca <loisgabor@gmail.com>
2020-11-02 10:29:11 -0500

    Added some files

--- index.js
+++ index.js
@@ -1,49 +1,55 @@
 // array of questions for user
 const inquirer = require("inquirer");
+const fs = require("fs");
+i = 0;
 
 const questions = [
   {
     type: "input",
     message: "What is your project title?",
-    name: title,
+    name: "title",
   },
   {
     type: "input",
     message: "Give a description of your project.",
-    name: description,
+    name: "description",
   },
   {
     type: "input",
     message: "Insert your table of contents",
-    name: contents,
+    name: "contents",
   },
   {
     type: "input",
     message: "Input installation guidelines",
-    name: installation,
+    name: "installation",
   },
   {
     type: "input",
     message: "Insert instructions for use",
-    name: usage,
+    name: "usage",
   },
   {
     type: "input",
     message: "Insert collaborator with links to their GitHub profile",
-    name: credits,
+    name: "credits",
   },
   {
     type: "input",
     message: "Insert your license",
-    name: license,
+    name: "license",
   },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+fs.writeFile("README2.md", questions[0].message, (err) =>
+  err ? console.log(err) : console.log("Success")
+);
 
 // function to initialize program
 function init() {}
+
+inquirer.prompt(questions);
 
 // function call to initialize program
 init();
