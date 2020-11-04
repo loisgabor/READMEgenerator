56c492e8fe996f983891d886318b5ce60f192398
Lois Simonca <loisgabor@gmail.com>
2020-11-04 18:27:06 -0500

    added badge

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const { clear } = require("console");
 const questions = [
   {
     type: "input",
+    message: "What is your name?",
+    name: "nameOne",
+  },
+  {
+    type: "input",
     message: "What is your project title?",
     name: "title",
   },
@@ -15,11 +20,6 @@ const questions = [
     type: "input",
     message: "Give a description of your project.",
     name: "description",
-  },
-  {
-    type: "input",
-    message: "Insert your table of contents",
-    name: "contents",
   },
   {
     type: "input",
@@ -32,14 +32,30 @@ const questions = [
     name: "usage",
   },
   {
-    type: "input",
-    message: "Insert collaborator with links to their GitHub profile",
-    name: "credits",
+    type: "list",
+    message: "Use arrow keys to select license options",
+    name: "license",
+    choices: ["MIT", "Apache"],
   },
   {
     type: "input",
-    message: "Insert your license",
-    name: "license",
+    message: "List the names of everyone who contributed to this project.",
+    name: "contribute",
+  },
+  {
+    type: "input",
+    message: "Write the test guidelines for your project",
+    name: "test",
+  },
+  {
+    type: "input",
+    message: "Write your email address to be displayed for any questions",
+    name: "questions",
+  },
+  {
+    type: "input",
+    message: "What is your github username",
+    name: "github",
   },
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,17 +1,76 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-# ${data.description}
-# ${data.contents}`;
+const currentDate = new Date().getFullYear();
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
 }
+function generateMarkdown(data) {
+  let license;
+  switch (data.license) {
+    case "MIT":
+      license = `Copyright [${currentDate}] [${data.nameOne}]
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
 
-//   return;
-//   {
-//     data.title;
-//   }
-//   {
-//     data.description;
-//   }
-// }
+      break;
+    case "Apache":
+      license = `Copyright [${currentDate}] [${data.nameOne}]
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+  https://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.`;
+      break;
+    default:
+      license = `Copyright [${currentDate}] [${data.nameOne}]
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`;
+      break;
+  }
+  return `
+# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#License)
+* [Contributing](#Contributing)
+* [Test](#test)
+* [Question](#Question)
+
+## Installation
+${data.installation}
+
+## Usage
+ ${data.usage}
+
+## License
+ 
+${renderLicenseBadge(license)}
+
+
+## Contributing
+ ${data.contribute}
+
+## Test
+${data.test}
+
+## Question
+${data.questions}
+
+Github user name: ${data.github}
+https://github.com/${data.github}`;
+}
 
 module.exports = generateMarkdown;
